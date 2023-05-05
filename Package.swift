312e80fd1ea5353596748137fd43e2baca703ce3
Soori <soori@sudheeksha.co>
2023-05-05 14:30:09 +0530

    updated

--- Package.swift
+++ Package.swift
@@ -19,13 +19,10 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AEWalletSDK",
             dependencies: []),
-        .binaryTarget(name: "TapSdk", path: "artifacts/TapSdk.xcframework"),
-       
+        .binaryTarget(name: "SSSSS", path: "artifacts/TapSdk.xcframework"),
         .testTarget(
             name: "AEWalletSDKTests",
             dependencies: ["AEWalletSDK"]),
