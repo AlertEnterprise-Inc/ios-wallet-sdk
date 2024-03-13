4f651533ec7acccdf23c6ff740625c9b32ef3e37
GaurAks <akshgaur93@gmail.com>
2024-03-13 15:58:11 +0530

    to add origo framework.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AEWalletSDK",
-            targets: ["AEWalletSDK","TapSdk"]),
+            targets: ["AEWalletSDK","TapSdk","OrigoSDK","SeosMobileKeysSDK","BerTlv","CocoaLumberjack","JSONModel","Mixpanel"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,6 +20,12 @@ let package = Package(
             name: "AEWalletSDK",
             dependencies: []),
         .binaryTarget(name: "TapSdk", path: "artifacts/TapSdk.xcframework"),
+        .binaryTarget(name: "SeosMobileKeysSDK", path: "Resources/SeosMobileKeysSDK.xcframework"),
+        .binaryTarget(name: "OrigoSDK", path: "Resources/OrigoSDK.xcframework"),
+        .binaryTarget(name: "BerTlv", path: "Resources/Release-universal/BerTlv.xcframework"),
+        .binaryTarget(name: "CocoaLumberjack", path: "Resources/Release-universal/CocoaLumberjack.xcframework"),
+        .binaryTarget(name: "JSONModel", path: "Resources/Release-universal/JSONModel.xcframework"),
+        .binaryTarget(name: "Mixpanel", path: "Resources/Release-universal/Mixpanel.xcframework"),
         .testTarget(
             name: "AEWalletSDKTests",
             dependencies: ["AEWalletSDK"]),
